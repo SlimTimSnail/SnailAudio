c8e021a074b8de58f34075573408761ef9f12ea5
TimDonnan <t.donnan@live.com.au>
2026-06-04 00:28:52 +1200

    plugin now makes a settings config file, not sure if I'll actually use it but lets see

--- addons/SnailAudio/SnailAudioPlugin.cs
+++ addons/SnailAudio/SnailAudioPlugin.cs
@@ -3,6 +3,7 @@
 // -----------------------------------------------------------------------
 
 #if TOOLS
+using System.Reflection.Metadata;
 using Godot;
 
 [Tool]
@@ -10,14 +11,33 @@ public partial class SnailAudioPlugin : EditorPlugin
 {
 	private const string _autoloadName = "AudioManager";
 
+	private const string _configPath = "res://addons/SnailAudio/SnailSettings.cfg";
+
 	public override void _EnterTree()
 	{
 		AddAutoloadSingleton(_autoloadName, "res://addons/SnailAudio/Scripts/AudioManager.cs");
+		
+		if (!FileAccess.FileExists(_configPath))
+		{
+			CreateConfig();
+		}
 	}
 
 	public override void _ExitTree()
 	{
 		RemoveAutoloadSingleton(_autoloadName);
+	}
+
+
+	private void CreateConfig ()
+	{
+		ConfigFile config = new();
+		config.SetValue("Player1", "player_name", "Bob");
+		config.SetValue("Player1", "best_score", 10);
+		config.SetValue("Player2", "player_name", "Bob2");
+		config.SetValue("Player2", "best_score", 9001);
+
+		config.Save(_configPath);
 	}
 }
 #endif
